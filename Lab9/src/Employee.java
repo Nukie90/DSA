dcae57ff6819c6d806414aa55e00493aad515c36
Intutch Sribanyaranond <65011306@kmitl.ac.th>
2023-09-15 14:08:12 +0700

    Lab9 Done

---
+++ Lab9/src/Employee.java
@@ -0,0 +1,16 @@
+
+public class Employee {
+    int salary;
+    private String name;
+
+    //constutor
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp " + name + "(" + salary + ")";
+    }
+}
